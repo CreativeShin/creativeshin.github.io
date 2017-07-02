165f919eb189b11421395e6bd62ab617907beb5f
CreativeShin <creative-shin@outlook.com>
2017-07-02 18:43:14 +0530

    changed iframe to thumbs

--- script/script.js
+++ script/script.js
@@ -1,22 +1,7 @@
+$(document).ready(function(){
+  // Changing the defaults
+window.sr = ScrollReveal({ reset: true });
 
-
-var a = document.querySelectorAll('a');
-var slide = document.querySelectorAll('.slide');
-a[0].addEventListener("mouseenter", function(){
-  slide[0].style.width = "70px";
-});
-a[0].addEventListener("mouseleave", function(){
-  slide[0].style.width = "0";
-});
-a[1].addEventListener("mouseenter", function(){
-  slide[1].style.width = "125px";
-});
-a[1].addEventListener("mouseleave", function(){
-  slide[1].style.width = "0";
-});
-a[2].addEventListener("mouseenter", function(){
-  slide[2].style.width = "70px";
-});
-a[2].addEventListener("mouseleave", function(){
-  slide[2].style.width = "0";
+// Customizing a reveal set
+sr.reveal('.transition', { duration: 200 });
 });
